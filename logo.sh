d3937c4cc64c350941470722680b736f3e1f8da1
Aomine <161364808+AomineAether@users.noreply.github.com>
2024-09-13 14:14:02 +0700

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 GREEN="\e[32m"
-AE='\033[0m'
 
 echo -e "${GREEN}"
 echo "  ###  #####  "
@@ -13,4 +12,4 @@ echo "##  ##  ##  "
 echo "##  ## #######"
 echo ""
 echo "Join our discord https://discord.gg/aetherealco"
-echo -e "${AE}"
+echo -e "\e[0m"
